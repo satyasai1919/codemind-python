2697bcc0aa574edb0bd59cc3267b4575ad8adfda
satyasai1919 <84486457+satyasai1919@users.noreply.github.com>
2022-08-21 14:47:37 +0530

    n=int(input()) s=n**2 sd=0 while s>0:     a=s%10     sd=sd+a     s=s//10 if(n==sd):     print('Neon Number') else:     print('Not Neon Number')

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,10 +1,11 @@
 n=int(input())
-s=n**2 
+s=n**2
 sd=0
 while s>0:
-    sd=sd+s%10
-    s=s//10 
-if (n==sd):
-    print("Neon Number")
+    a=s%10
+    sd=sd+a
+    s=s//10
+if(n==sd):
+    print('Neon Number')
 else:
-    print("Not Neon Number")
+    print('Not Neon Number')
