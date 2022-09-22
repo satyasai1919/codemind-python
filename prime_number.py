8ef4bd7a6a8c71901c62c23dbfd301453c3047b3
satyasai1919 <84486457+satyasai1919@users.noreply.github.com>
2022-09-22 20:55:01 +0530

    num = int(input()) y=num//2 if num > 1:     for i in range(2,y+1):         if (num % i) == 0:             print("not a prime")             break     else:         print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,6 +1,7 @@
 num = int(input())
+y=num//2
 if num > 1:
-    for i in range(2, int(num/2)+1):
+    for i in range(2,y+1):
         if (num % i) == 0:
             print("not a prime")
             break
